8be8dee526b1c45b7498ed2d555216e9108289fd
Matvey Seregin <daymatt@mail.ru>
2024-03-20 15:02:02 +0700

    add app color palette

--- app/src/main/java/dev/lantt/wordsfactory/ui/theme/Color.kt
+++ app/src/main/java/dev/lantt/wordsfactory/ui/theme/Color.kt
@@ -9,3 +9,16 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// App palette
+val InkDark = Color(0xFF3C3A36)
+val InkDarkGray = Color(0xFF78746D)
+val InkGray = Color(0xFFBEBAB3)
+val InkWhite = Color(0xFFFFFFFF)
+
+val PrimaryColor = Color(0xFFE3562A)
+val SecondaryColor = Color(0xFF65AAEA)
+
+val SuccessColor = Color(0xFF5BA092)
+val ErrorColor = Color(0xFFEF4949)
+val WarningColor = Color(0xFFF2A03F)
