1a60d48be054fe63353a31c431b25d1e4cc30143
Matvey Seregin <daymatt@mail.ru>
2024-03-20 14:59:04 +0700

    add fonts and text styles

---
+++ app/src/main/java/dev/lantt/wordsfactory/ui/theme/Font.kt
@@ -0,0 +1,13 @@
+package dev.lantt.wordsfactory.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import dev.lantt.wordsfactory.R
+
+val Rubik = FontFamily(
+    Font(R.font.rubik_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.rubik_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.rubik_bold, FontWeight.Bold, FontStyle.Normal),
+)
---
+++ app/src/main/java/dev/lantt/wordsfactory/ui/theme/TextStyle.kt
@@ -0,0 +1,71 @@
+package dev.lantt.wordsfactory.ui.theme
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val HeadingH1 = TextStyle(
+    fontFamily = Rubik,
+    fontWeight = FontWeight.Bold,
+    fontSize = 56.sp
+)
+
+val HeadingH2 = TextStyle(
+    fontFamily = Rubik,
+    fontWeight = FontWeight.Bold,
+    fontSize = 40.sp
+)
+
+val HeadingH3 = TextStyle(
+    fontFamily = Rubik,
+    fontWeight = FontWeight.Bold,
+    fontSize = 32.sp
+)
+
+val HeadingH4 = TextStyle(
+    fontFamily = Rubik,
+    fontWeight = FontWeight.Medium,
+    fontSize = 24.sp
+)
+
+val HeadingH5 = TextStyle(
+    fontFamily = Rubik,
+    fontWeight = FontWeight.Medium,
+    fontSize = 20.sp
+)
+
+val ParagraphLarge = TextStyle(
+    fontFamily = Rubik,
+    fontWeight = FontWeight.Normal,
+    fontSize = 16.sp
+)
+
+val ParagraphMedium = TextStyle(
+    fontFamily = Rubik,
+    fontWeight = FontWeight.Normal,
+    fontSize = 14.sp
+)
+
+val ParagraphSmall = TextStyle(
+    fontFamily = Rubik,
+    fontWeight = FontWeight.Medium,
+    fontSize = 12.sp
+)
+
+val ButtonLarge = TextStyle(
+    fontFamily = Rubik,
+    fontWeight = FontWeight.Medium,
+    fontSize = 18.sp
+)
+
+val ButtonMedium = TextStyle(
+    fontFamily = Rubik,
+    fontWeight = FontWeight.Medium,
+    fontSize = 16.sp
+)
+
+val ButtonSmall = TextStyle(
+    fontFamily = Rubik,
+    fontWeight = FontWeight.Medium,
+    fontSize = 14.sp
+)
