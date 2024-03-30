45c37c021dd3be193db2a79539c130f76851f097
lantt <lanttoff@gmail.com>
2024-03-30 17:29:22 +0500

    add annotated string

--- app/src/main/java/dev/lantt/wordsfactory/dictionary/presentation/components/WordMeaning.kt
+++ app/src/main/java/dev/lantt/wordsfactory/dictionary/presentation/components/WordMeaning.kt
@@ -4,11 +4,8 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,13 +13,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import dev.lantt.wordsfactory.R
 import dev.lantt.wordsfactory.core.presentation.ui.theme.CornerRadiusMedium
 import dev.lantt.wordsfactory.core.presentation.ui.theme.InkGray
 import dev.lantt.wordsfactory.core.presentation.ui.theme.PaddingMedium
 import dev.lantt.wordsfactory.core.presentation.ui.theme.PaddingSmall
-import dev.lantt.wordsfactory.core.presentation.ui.theme.PaddingTiny
 import dev.lantt.wordsfactory.core.presentation.ui.theme.ParagraphLarge
 import dev.lantt.wordsfactory.core.presentation.ui.theme.ParagraphMedium
 import dev.lantt.wordsfactory.core.presentation.ui.theme.PrimaryColor
@@ -60,24 +59,20 @@ fun WordMeaning(
                 color = Color.Black
             )
 
-            // TODO spannable
             val example = meaning.definition.example
             example?.let {
-                Row {
-                    Text(
-                        text = stringResource(id = R.string.example),
-                        style = ParagraphMedium,
-                        color = SecondaryColor
-                    )
-
-                    Spacer(modifier = Modifier.width(PaddingTiny))
-
-                    Text(
-                        text = example,
-                        style = ParagraphMedium,
-                        color = Color.Black
-                    )
+                val exampleText = buildAnnotatedString {
+                    withStyle(style = SpanStyle(color = SecondaryColor)) {
+                        append(stringResource(id = R.string.example) + " ")
+                    }
+                    append(example)
                 }
+
+                Text(
+                    text = exampleText,
+                    style = ParagraphMedium,
+                    color = Color.Black
+                )
             }
         }
     }
